f0681ac2a15897f4f789e1390bb7a0c20e25c713
Giorgos Athanasopoulos <90207655+GiorgosAthanasopoulos@users.noreply.github.com>
2023-12-04 00:37:07 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#Install dependencies
+sudo apt install git
+sudo apt install build-essential
+sudo apt install libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev
+
+#Clone and build raylib
+git clone https://github.com/raysan5/raylib.git raylib
+cd raylib/src/
+make PLATFORM=PLATFORM_DESKTOP RAYLIB_LIBTYPE=SHARED # To make the dynamic shared version.
+sudo make install RAYLIB_LIBTYPE=SHARED # Dynamic shared version.
+cd ../../
+rm -rf raylib
+
+#Clone and run game
+git clone https://github.com/giorgosathanasopoulos/raylib-chess
+cd raylib-chess
+g++ nobuild.c -o nobuild
+./nobuild
